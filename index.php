e0c4cf481e56a281539f11467848e43762877f8f
JahresPeterD <jahrespeter@gmail.com>
2021-05-03 15:38:27 -0400

    Update: Adjusted Index Page links

--- index.php
+++ index.php
@@ -18,10 +18,10 @@ include("auth.php");
         <a href="index.php"><font size=2><img class="homeicon" src="images\home.png" height="27" width="27"></font>Home</img></a>
       </li>
       <li>
-        <a href="about.html"><font size=2><img class="homeicon" src="images\about.png" height="27" width="27"></font>About</img></a>
+        <a href="about.php"><font size=2><img class="homeicon" src="images\about.png" height="27" width="27"></font>About</img></a>
       </li>
       <li>
-        <a href="polls.html"><font size=2><img class="homeicon" src="images\poll.png" height="27" width="27"></font>Polls</img></a>
+        <a href="polls.php"><font size=2><img class="homeicon" src="images\poll.png" height="27" width="27"></font>Polls</img></a>
       </li>
       <li>
         <a href="login.php"><font size=2><img class="homeicon" src="images\friend.png" height="27" width="27"></font>Login</img></a>
@@ -30,10 +30,10 @@ include("auth.php");
         <a href="signup.php"><font size=2><img class="homeicon" src="images\signup.png" height="27" width="27"></font>Sign Up</img></a>
       </li>
       <li>
-        <a href="canteen.html"><font size=2><img class="homeicon" src="images\canteen.png" height= "27" width= "27"></font>Canteen Menu</img></a>
+        <a href="canteen.php"><font size=2><img class="homeicon" src="images\canteen.png" height= "27" width= "27"></font>Canteen Menu</img></a>
       </li>
             <li>
-        <a href="busroute.html"><font size=2><img src="images\busroute.png" height="27" width="27"></font>Bus Route</img></a>
+        <a href="busroute.php"><font size=2><img src="images\busroute.png" height="27" width="27"></font>Bus Route</img></a>
       </li>
       <li>
         <a href="contact.html"><font size=2><img class="homeicon" src="images\contact.png" height= "27" width="27"></font>Contact<img></a>
